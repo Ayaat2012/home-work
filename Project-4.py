ff75c1d90c40b56d46234b51b79ae1705361656a
Ayaat2012 <iffatrajshahi@gmail.com>
2025-08-30 19:35:29 +0600

    initial commit

--- Project-4.py
+++ Project-4.py
@@ -1,2 +1,14 @@
+class Adder:
+    def __init__(self, num1, num2):
+        self.number1 = num1
+        self.number2 = num2
 
+    def add_numbers(self):
+        return self.number1 + self.number2
+
+my_adder = Adder(10, 5)
+
+sum_result = my_adder.add_numbers()
+
+print(f"The sum is: {sum_result}")
 
